4d627d02926eb57743f41c278e33951555f8eaa0
EndlessSummer29 <995867553@qq.com>
2023-11-16 17:48:57 +0800

    配置axios并测试

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// Axios
+import api from './api'
 
 const app = createApp(App)
 
+app.provide('$axios', api)
 app.use(createPinia())
 app.use(router)
 
